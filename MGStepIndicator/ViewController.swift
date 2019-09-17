f6e884b9228bb8f9aba5d35eea44e837453f1dc5
mustafagunes <gunes149@gmail.com>
2019-09-17 15:20:44 +0300

    first commit

---
+++ MGStepIndicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MGStepIndicator
+//
+//  Created by Mustafa GUNES on 17.09.2019.
+//  Copyright © 2019 Mustafa GUNES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
